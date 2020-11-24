09a6d2f17d1b106c4c446e1c40f87c8ead071f80
Donald Zullick <donald@creativecomplement.com>
2020-11-24 11:45:51 -0600

    minor edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <div className="footer">
       <footer className="py-2 bg-dark fixed-bottom">
         <div className="container">
-          <p className="m-0 text-center color: #b2beb5">
+          <p className="m-0 text-center creative-complement-text">
             Copyright &copy; Creative Complement {new Date().getFullYear()}
           </p>
         </div>
